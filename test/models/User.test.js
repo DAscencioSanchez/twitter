6ce82565e7a3fd5aa382f908576e5321d221a2be
DAscencioSanchez <diaspm77@gmail.com>
2022-04-21 15:54:41 -0500

    Adding a test to user class, passed

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,7 +2,12 @@ const User = require('./../../app/models/User')
 
 describe("Prueva de jest ", () => {
     test('Prueba 1', () => {
-        const user = new User(1, "carlogilmar", "Carlo", "Bio")
-        expect(user.name).toBe("carlogilmar")
+        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "dateUpdated")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("dateUpdated")
     })
 })
