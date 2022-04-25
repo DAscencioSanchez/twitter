4de3fdf164ce6450afeff717fa7930b641937230
DAscencioSanchez <diaspm77@gmail.com>
2022-04-25 14:09:55 -0500

    Practica Sprint 3. Requerimento 3: Regresar un error cuando se trate de crear un usuario con un payload con propiedades faltantes

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,17 +1,24 @@
-const Userview = require('./../../app/views/UserView')
+const UserView = require('./../../app/views/UserView')
 
 describe("Tests for UserView", () => {
 
-    test("Return an error object when try to create a new user in a null payload", () => {
+    test("1. Return an error object when try to create a new user in a null payload", () => {
         const payload = null
-        const result = Userview.createUser(payload)
+        const result = UserView.createUser(payload)
 
         expect(result.error).toMatch(/payload no existe/)
     })
 
-    test("Return an error objecto when try to create a new user with a payload with invalid properties", () => {
+    test("2. Return an error objecto when try to create a new user with a payload with invalid properties", () => {
         const payload = {username: null, name: 12, id: "id"}
-        const result = Userview.createUser(payload)
-        expect(result.error).toMatch(/necesita tener un valor valido/)
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
     })
+
+    test("3. Return an error objecto when try to create a new user with a payload with missing properties ", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
 })
