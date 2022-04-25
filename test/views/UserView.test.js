a5cdf8bffe529e8f0ef5cfcf61e762b0bead5763
DAscencioSanchez <diaspm77@gmail.com>
2022-04-25 14:16:54 -0500

    Practica Sprint3. Requerimento 4: Crear un nuevo usuario cuando se ingresa una payload valida

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -9,16 +9,23 @@ describe("Tests for UserView", () => {
         expect(result.error).toMatch(/payload no existe/)
     })
 
-    test("2. Return an error objecto when try to create a new user with a payload with invalid properties", () => {
+    test("2. Return an error object when try to create a new user with a payload with invalid properties", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor valido/)
     })
 
-    test("3. Return an error objecto when try to create a new user with a payload with missing properties ", () => {
+    test("3. Return an error object when try to create a new user with a payload with missing properties ", () => {
         const payload = {username: "Username"}
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor valido/)
     })
 
+    test("4. Create a user when given a valid payload ", () => {
+        const payload = {username: "username", id:1, name: "name"}
+        const result = UserView.createUser(payload)
+        expect(result.name).toBe("name")
+        expect(result.username).toBe("username")
+        expect(result.id).toBe(1)
+    })
 })
